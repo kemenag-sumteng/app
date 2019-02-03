cc903954f1e0e7aff580d38ea2cecd813d881862
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-02-03 20:08:56 +0800

    Add validations to EDataKeagamaanKatolik

--- app/models/e_data_keagamaan_katolik.rb
+++ app/models/e_data_keagamaan_katolik.rb
@@ -1,3 +1,5 @@
 class EDataKeagamaanKatolik < ApplicationRecord
 	belongs_to :user
+	validates :judul, presence: true, length: { minimum: 5, maximum: 255 }
+	validates :keterangan, presence: true, length: { minimum: 5}
 end
