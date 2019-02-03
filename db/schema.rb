15af0c6795d6eec61c21862a88ee12eaf614f660
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-02-03 19:03:46 +0800

    Removed nama from Devise

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2019_02_01_194111) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
