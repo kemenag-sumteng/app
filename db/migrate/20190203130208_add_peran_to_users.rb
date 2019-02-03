8222e9c09a616617018ffb073a901179c8a12fe7
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-02-03 21:03:49 +0800

    Set default status to peran

--- db/migrate/20190203130208_add_peran_to_users.rb
+++ db/migrate/20190203130208_add_peran_to_users.rb
@@ -1,12 +1,12 @@
 class AddPeranToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :super_admin, :boolean
-    add_column :users, :kasie_urakat, :boolean
-    add_column :users, :penyelenggara_pendakat, :boolean
-    add_column :users, :pegawai_urakat, :boolean
-    add_column :users, :pegawai_pendakat, :boolean
-    add_column :users, :anggota_urakat, :boolean
-    add_column :users, :anggota_pendakat, :boolean
-    add_column :users, :pengunjung, :boolean
+    add_column :users, :super_admin, :boolean, default: false
+    add_column :users, :kasie_urakat, :boolean, default: false
+    add_column :users, :penyelenggara_pendakat, :boolean, default: false
+    add_column :users, :pegawai_urakat, :boolean, default: false
+    add_column :users, :pegawai_pendakat, :boolean, default: false
+    add_column :users, :anggota_urakat, :boolean, default: false
+    add_column :users, :anggota_pendakat, :boolean, default: false
+    add_column :users, :pengunjung, :boolean, default: true
   end
 end
