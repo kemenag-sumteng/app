e287b427b4f043bc6ede03b9b589d9729627a97d
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-02-03 18:40:34 +0800

    Fix halaman with utama action test

--- spec/controllers/halaman_controller_spec.rb
+++ spec/controllers/halaman_controller_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe HalamanController, :type => :controller do
   render_views
 
   describe "GET index" do
-    it "has a greeting" do
+    it "has a greeting Title and Text" do
       get :utama
-      page.should have_selector('h1', text: 'Salve')
+      expect(response.body).to match /<h1 class="card-header">Salve/
+      expect(response.body).to match /<h5 class="card-title">Selamat datang di Bimas Katolik Sumba Tengah./
+      expect(response.body).to match /<p class="card-text">Untuk memulai, Anda dapat memilih salah satu dari menu yang tersedia./
     end
   end
 end
